2c4d2240baef683e5a16abbd9b733221c60d7d23
Rohit Gupta <rohitgupta8517@gmail.com>
2026-06-10 09:32:53 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "EMPTY_LIST"
+        encoded = "🐵".join(strs)
+        return encoded[::-1]
+
+    def decode(self, s: str) -> List[str]:
+        if s == "EMPTY_LIST":
+            return []
+            
+        if s == "":
+            return [""]
+            
+        encoded = s[::-1]
+        decoded = encoded.split("🐵")
+        return list(decoded)
