95dfcade26a89ff911010f4048954961e14eabbb
Rohit Gupta <rohitgupta8517@gmail.com>
2026-06-18 11:03:35 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,7 @@
+# revision
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            return Counter(s) == Counter(t)
